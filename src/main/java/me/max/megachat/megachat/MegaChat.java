cb71611e0b72e15d901ba377a8672a7ba19dfc91
MaxiMiniJaniJos <maxberkelmans@live.nl>
2018-01-11 22:32:37 +0100

    Initial commit

---
+++ src/main/java/me/max/megachat/megachat/MegaChat.java
@@ -0,0 +1,17 @@
+package me.max.megachat.megachat;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MegaChat extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
