20378690c7af89db521d9d08af2b3f758663b6f7
MaxiMiniJaniJos <maxberkelmans@live.nl>
2018-01-11 22:51:02 +0100

    Created develop branch. Add config and messages files for future. Added in basic logging methods.

--- src/main/java/me/max/megachat/megachat/MegaChat.java
+++ src/main/java/me/max/megachat/megachat/MegaChat.java
@@ -7,11 +7,24 @@ public final class MegaChat extends JavaPlugin {
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        saveDefaultConfig();
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        saveConfig();
+    }
+
+    public void info(String info){
+        this.getLogger().info(info);
+    }
+
+    public void warning(String warning){
+        this.getLogger().warning(warning);
+    }
+
+    public void error(String error){
+        this.getLogger().severe(error);
     }
 }
